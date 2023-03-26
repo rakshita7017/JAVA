c6642e99ed51ef1408cee7a33c4d27c19e31c0ce
rakshita087 <116157841+rakshita087@users.noreply.github.com>
2023-03-26 21:55:36 +0530

    Add files via upload

---
+++ Linearsearch.java
@@ -0,0 +1,15 @@
+public class Linearsearch {
+    public static void main(String[] args) {
+        int arr[]={12,78,45,3,9,2,8};
+        int n=arr.length;
+        int item=9;
+        for(int i=0;i<n;i++){
+            if(arr[i]==item){
+                System.out.println(("item is present at position:"+i));
+            }
+    
+            
+        }
+    }
+    
+}
