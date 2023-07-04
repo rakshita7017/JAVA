e9cb513b65a0b5dd28d6bfb2794e0b8d61aab5e0
rakshita087 <116157841+rakshita087@users.noreply.github.com>
2023-07-04 15:34:45 +0530

    Add files via upload

---
+++ Pairsinanarray.java
@@ -0,0 +1,18 @@
+public class Pairsinanarray {
+    public static void pairs(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            int curr=arr[i];
+            for(int j=i+1;j<arr.length;j++){
+            System.out.print("("+curr+","+arr[j]+")");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+        int arr[]={2,4,6,8,10};
+        pairs(arr);
+
+    }
+    
+}
