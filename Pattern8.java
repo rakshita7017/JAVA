8d939ae1856986383d3b19132a8e5de8c7e067eb
rakshita087 <116157841+rakshita087@users.noreply.github.com>
2023-07-05 12:15:44 +0530

    Add files via upload

---
+++ Pattern8.java
@@ -0,0 +1,25 @@
+public class Pattern8 {
+    public static void printno(int n){
+        for(int row=1;row<=n;row++){
+            
+            for( int i=1;i<=n-row;i++){
+                System.out.print(" ");
+
+            }
+            for(int col=row;col>=1;col--){
+                System.out.print(col+"");
+            }
+        
+            
+            for(int col=2;col<=row;col++){
+                System.out.print(col+"");
+            }
+  System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+        printno(5);
+    }
+    
+}
